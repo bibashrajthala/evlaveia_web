19b67201180c33385d8dd588f611a532996801b4
bibash rajthala <bibashrajthala10@gmail.com>
2024-02-20 11:14:47 +0545

    project setup with eslint, prettier, tailwind, husky

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline border border-red-900">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
